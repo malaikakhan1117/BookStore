c6574b66b38df27ed125790c608d262ca878ed37
malaikakhan1117 <maliaqakhan@gmail.com>
2024-03-27 16:46:57 +0500

    first commit

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb://localhost:27017/books-collection";
+export const mongoDBURL = "mongodb://localhost:27017/booksCollection";
 
 
 // Please create a free database for yourself.
